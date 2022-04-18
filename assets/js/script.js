b81be0c790f7af2107e5a875f362ccb6338b046e
Manoel Arruda <manoelarruda@outlook.com.br>
2022-04-18 15:27:47 -0300

    pouplacao

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,29 @@
-fetch('https://restcountries.com/v2/all').then(response => response.json())
-.then(data => {
-  console.log(data);
-  /* alemanha */
+const url = fetch('https://restcountries.com/v2/all');
+
+url.then((response) => response.json())
+.then((body) => {
+  console.log(body);
+  const titleCountries = document.querySelectorAll('.titleCountries');
+    
+    body.forEach((ex) => {
+      
+      titleCountries.forEach((e) => {
+        const nameCountries = e.textContent;
+        
+          if(ex.name === nameCountries){
+            console.log(nameCountries, ex.population);
+            document.getElementById("population").innerHTML = ex.population;
+          }
+    })   
+  })  
+})
+  /*alemanha 
   document.getElementById("pgermany").innerHTML = data[84].population;
   document.getElementById("rgermany").innerHTML = data[84].region;
   document.getElementById("cgermany").innerHTML = data[84].capital;
   
-  /* usa */
+  usa
   document.getElementById("pusa").innerHTML = data[239].population;
   document.getElementById("rusa").innerHTML = data[239].region;
   document.getElementById("cusa").innerHTML = data[239].capital;
-  
-});
+*/
