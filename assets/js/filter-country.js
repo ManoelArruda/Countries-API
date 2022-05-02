ac8bb8164018f9ae526b42a03b1bfd241ce80132
Manoel Arruda <manoelarruda@outlook.com.br>
2022-05-02 20:51:26 -0300

    Refatoração do dark-mode

--- assets/js/filter-country.js
+++ assets/js/filter-country.js
@@ -1,4 +1,7 @@
-/* let oi = document.querySelectorAll('.titleCountries');
-oi.forEach((item) => {
-  console.log(item.textContent);
-}) */
+function filterCountry(){
+  const titleCountries = document.querySelectorAll('.titleCountries');
+  
+  titleCountries.forEach((item) => {
+    console.log(item.textContent);
+  })
+}
