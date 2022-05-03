f06e3218d6bf7b584c83171fec4691747af589f9
Manoel Arruda <manoelarruda@outlook.com.br>
2022-05-02 23:05:05 -0300

    Criação do filtro por país ao digitar o nome do país no input

--- assets/js/filter-country.js
+++ assets/js/filter-country.js
@@ -1,7 +1,28 @@
+const inputsearch = document.getElementById("inputsearch");
+inputsearch.addEventListener('keyup', filterCountry);
+
 function filterCountry(){
-  const titleCountries = document.querySelectorAll('.titleCountries');
+  const card = document.querySelectorAll(".card");  
+
+  card.forEach((item) => {
   
-  titleCountries.forEach((item) => {
-    console.log(item.textContent);
+    item.style.display = "none";
+  
+    const userCountry = document.getElementById("inputsearch").value;
+    const titleCountries = item.querySelector('.titleCountries').textContent;
+
+    if((userCountry === titleCountries.toUpperCase()) || 
+     (userCountry === titleCountries.toLowerCase()) ||
+     (userCountry === titleCountries)){
+        item.style.display = "block";
+        return;
+      }else if((userCountry === "") || (userCountry === " ")){
+        item.style.display = "block";
+        return;
+      }
+      /* item.style.display = (titleCountries === userCountry) 
+      ||(userCountry === titleCountries.toLowerCase()) 
+      || (userCountry === titleCountries.toUpperCase()) ? "block" : "none"; */
+      
   })
 }
